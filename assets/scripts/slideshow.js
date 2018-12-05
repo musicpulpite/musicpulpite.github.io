36a927c8970ebb77a70b95fcaf3f0e3619abdfc2
William McAdams <musicpulpite@gmail.com>
2018-12-05 17:34:49 -0500

    Add slidability on Projects panel

---
+++ assets/scripts/slideshow.js
@@ -0,0 +1,25 @@
+let orderArr = ['zeroth hidden', 'first', 'second', 'third', 'fourth hidden'];
+
+const slideRight = () => {
+  window.$projects.forEach((project) => {
+    orderIdx = orderArr.findIndex((order) => order === project.className);
+    project.className = orderArr[(orderIdx + 1) % orderArr.length];
+  });
+};
+
+const slideLeft = () => {
+  window.$projects.forEach((project) => {
+    orderIdx = orderArr.findIndex((order) => order === project.className);
+    let newIdx = orderIdx > 0 ? orderIdx - 1 : orderArr.length - 1;
+    project.className = orderArr[newIdx];
+  });
+};
+
+window.addEventListener('DOMContentLoaded', () => {
+  window.$projects = Array.from(document.getElementById('projects').children);
+  const leftButton = document.getElementById('left');
+  const rightButton = document.getElementById('right');
+
+  leftButton.addEventListener('click', slideLeft);
+  rightButton.addEventListener('click', slideRight);
+});

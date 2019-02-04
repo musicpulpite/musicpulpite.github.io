cac0fc1fc1cfdd2f18c099286b3a9d43a47fd01d
William McAdams <musicpulpite@gmail.com>
2019-02-04 15:02:52 -0500

    Update project slides styling

--- assets/scripts/slideshow.js
+++ assets/scripts/slideshow.js
@@ -1,25 +1,25 @@
-let orderArr = ['zeroth hidden', 'first', 'second', 'third', 'fourth hidden'];
+let orderArr = ['zeroth hidden', 'first', 'second', 'third', 'fourth hidden', 'fifth hidden'];
 
 const slideRight = () => {
-  window.$projects.forEach((project) => {
-    orderIdx = orderArr.findIndex((order) => order === project.className);
+  projects.forEach((project) => {
+    let orderIdx = orderArr.findIndex((order) => order === project.className);
     project.className = orderArr[(orderIdx + 1) % orderArr.length];
   });
 };
 
 const slideLeft = () => {
-  window.$projects.forEach((project) => {
-    orderIdx = orderArr.findIndex((order) => order === project.className);
+  projects.forEach((project) => {
+    let orderIdx = orderArr.findIndex((order) => order === project.className);
     let newIdx = orderIdx > 0 ? orderIdx - 1 : orderArr.length - 1;
     project.className = orderArr[newIdx];
   });
 };
 
 window.addEventListener('DOMContentLoaded', () => {
-  window.$projects =
+  projects =
   Array.from(
-    document.getElementById('projects').children
-  ).slice(3);
+    document.getElementById('project-slideshow').children
+  );
   const leftButton = document.getElementById('left');
   const rightButton = document.getElementById('right');
 
